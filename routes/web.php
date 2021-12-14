0aeb3eb1b3356909f849317aa9bfe781cec9fd3a
basia <barbara.k.kowol@gmail.com>
2021-12-14 16:51:55 +0100

    feature/authorisation routes secured by middleware

--- routes/web.php
+++ routes/web.php
@@ -19,31 +19,34 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/places', 'App\Http\Controllers\PlaceController@index')->name('placeIndex');
-Route::get('/places/create', 'App\Http\Controllers\PlaceController@create')->name('placeCreate');
-Route::post('/places', 'App\Http\Controllers\PlaceController@store')->name('placeStore');
-Route::get('/places/{place}', 'App\Http\Controllers\PlaceController@show')->name('placeShow');
-Route::get('/places/{place}/edit', 'App\Http\Controllers\PlaceController@edit')->name('placeEdit');
-Route::put('/places/{place}', 'App\Http\Controllers\PlaceController@update')->name('placeUpdate');
-Route::delete('/places/{place}', 'App\Http\Controllers\PlaceController@destroy')->name('placeDestroy');
+    Route::get('/places', 'App\Http\Controllers\PlaceController@index')->name('placeIndex');
+    Route::get('/places/create', 'App\Http\Controllers\PlaceController@create')->name('placeCreate');
+    Route::post('/places', 'App\Http\Controllers\PlaceController@store')->name('placeStore');
+    Route::get('/places/{place}', 'App\Http\Controllers\PlaceController@show')->name('placeShow');
+    Route::get('/places/{place}/edit', 'App\Http\Controllers\PlaceController@edit')->name('placeEdit');
+    Route::put('/places/{place}', 'App\Http\Controllers\PlaceController@update')->name('placeUpdate');
+    Route::delete('/places/{place}', 'App\Http\Controllers\PlaceController@destroy')->name('placeDestroy');
 
-Route::get('/yachts', 'App\Http\Controllers\YachtController@index')->name('yachtIndex');
-Route::get('/yachts/{yacht}', 'App\Http\Controllers\YachtController@show')->name('yachtShow');
+    Route::get('/yachts', 'App\Http\Controllers\YachtController@index')->name('yachtIndex');
+    Route::get('/yachts/{yacht}', 'App\Http\Controllers\YachtController@show')->name('yachtShow');
 
-Route::get('/skippers', 'App\Http\Controllers\SkipperController@index')->name('skipperIndex');
-Route::get('/skippers/{skipper}', 'App\Http\Controllers\SkipperController@show')->name('skipperShow');
+    Route::get('/skippers', 'App\Http\Controllers\SkipperController@index')->name('skipperIndex');
+    Route::get('/skippers/{skipper}', 'App\Http\Controllers\SkipperController@show')->name('skipperShow');
 
-Route::get('/traffic', 'App\Http\Controllers\TrafficController@index')->name('trafficIndex');
-Route::get('/traffic/create', 'App\Http\Controllers\TrafficController@create')->name('trafficCreate');
-Route::post('/traffic', 'App\Http\Controllers\TrafficController@store')->name('trafficStore');
-Route::get('/traffic/{record}', 'App\Http\Controllers\TrafficController@show')->name('trafficShow');
-Route::get('/traffic/history/{filter}', 'App\Http\Controllers\TrafficController@showHistory')->name('trafficShowHistory');
-Route::get('/traffic/{record}/archive', 'App\Http\Controllers\TrafficController@archive')->name('trafficArchive');
+    Route::get('/traffic', 'App\Http\Controllers\TrafficController@index')->name('trafficIndex');
+    Route::get('/traffic/create', 'App\Http\Controllers\TrafficController@create')->name('trafficCreate');
+    Route::post('/traffic', 'App\Http\Controllers\TrafficController@store')->name('trafficStore');
+    Route::get('/traffic/{record}', 'App\Http\Controllers\TrafficController@show')->name('trafficShow');
+    Route::get('/traffic/history/{filter}', 'App\Http\Controllers\TrafficController@showHistory')->name('trafficShowHistory');
+    Route::get('/traffic/{record}/archive', 'App\Http\Controllers\TrafficController@archive')->name('trafficArchive');
 
-Route::get('/reservations', 'App\Http\Controllers\ReservationController@index')->name('reservationIndex');
-Route::post('/reservations/check-avaliable-places', 'App\Http\Controllers\ReservationController@checkAvaliablePlaces')->name('reservationCheckAvaliablePlaces');
-Route::get('/reservations/create/{place}/{dateOfCome}/{dateOfLeave}', 'App\Http\Controllers\ReservationController@create')->name('reservationCreate');
-Route::post('/reservations', 'App\Http\Controllers\ReservationController@store')->name('reservationStore');
-Route::get('/reservations/{record}', 'App\Http\Controllers\ReservationController@show')->name('reservationShow');
+    Route::get('/reservations', 'App\Http\Controllers\ReservationController@index')->name('reservationIndex');
+    Route::post('/reservations/check-avaliable-places', 'App\Http\Controllers\ReservationController@checkAvaliablePlaces')->name('reservationCheckAvaliablePlaces');
+    Route::get('/reservations/create/{place}/{dateOfCome}/{dateOfLeave}', 'App\Http\Controllers\ReservationController@create')->name('reservationCreate');
+    Route::post('/reservations', 'App\Http\Controllers\ReservationController@store')->name('reservationStore');
+    Route::get('/reservations/{record}', 'App\Http\Controllers\ReservationController@show')->name('reservationShow');
+});
